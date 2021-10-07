1bbce217af0f4550cf6e8a12a8870e34209f4134
Adriana Holley <ahollgoog@gmail.com>
2021-10-06 20:23:35 -0500

    first changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import facezoom from './face-zoom.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={facezoom} className="App-logo" alt="logo" />
+
+        <p>Adriana Holley</p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Android Engineer at Target
+        </a>         
+
+        <Button variant='outlined'>About</Button>
+        <Button variant='outlined'>Experience</Button>
+        <Button variant='outlined'>Projects</Button>     
+
+        <a>Work in Progress. Last updated Oct 6, 2021.</a>
+
       </header>
     </div>
   );
