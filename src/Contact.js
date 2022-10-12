999db88c91542d14300fd1a9b2915eb3cba516bf
Adriana Holley <ahollgoog@gmail.com>
2022-10-11 20:57:35 -0500

    add iframe

--- src/Contact.js
+++ src/Contact.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import { Card } from '@material-ui/core';
 
 class Contact extends Component {
     render() {
         return (
             <div>
-                <Card>
-                    hi
-                </Card>
-                <Card>
-                    hi
-                </Card>
-                <Card>
-                    hi
-                </Card>
-                <Card>
-                    hi
-                </Card>
+                <iframe
+                    src="https://5poo4rqyjy0795.embednotionpage.com/ACL-itinerary-54299f1b08a340d78041a5c94f9cbf21"
+                    width="100%"
+                    height="5000"
+                    title="ACL Itinerary Notion Page"
+                    // style="border: none; width: 100%; height: 500px; border: 2px solid #ccc; border-radius: 10px; padding: none;"
+                ></iframe>
+                
             </div>
         );
     } 
